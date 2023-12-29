52be36f9a58df4fe9dda154eb1af82dfc682788c
Vincent Dizon <workvpdizon@gmail.com>
2023-12-29 22:39:01 +0800

    added mantine

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -14,5 +14,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 }
