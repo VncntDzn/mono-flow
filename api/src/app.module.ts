6217113106c602c63e9ef42159c3f2e907323215
Vincent Dizon <workvpdizon@gmail.com>
2024-01-03 01:19:18 +0800

    added config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,18 +1,23 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 
+import db from '@/configs/db';
+import { ConfigModule } from '@nestjs/config';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FeaturesModule } from './features/features.module';
 
 @Module({
   imports: [
+    FeaturesModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(db()),
     ThrottlerModule.forRoot([
       {
         ttl: 15,
         limit: 5,
       },
     ]),
-    FeaturesModule,
   ],
   providers: [
     {
