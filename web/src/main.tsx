7e7ab8995d4569b586844ae97eab9c84c9feb8a6
Vincent Dizon <workvpdizon@gmail.com>
2024-01-02 21:21:07 +0800

    added react query

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,14 +1,21 @@
+import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { MantineProvider } from '@mantine/core';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.tsx';
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
     </MantineProvider>
   </React.StrictMode>,
 );
