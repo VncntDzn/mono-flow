9dece1d5291feec29acf591f1872454c441514fc
Vincent Dizon <workvpdizon@gmail.com>
2024-01-02 22:22:03 +0800

    added files for api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
 
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 15,
+        limit: 5,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
