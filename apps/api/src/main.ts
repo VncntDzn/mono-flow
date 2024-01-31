4aeda49fa652afe1ebee0f7f5f1b0d0e908d90a8
Vincent Dizon <workvpdizon@gmail.com>
2024-02-01 00:57:31 +0800

    added exception filter

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,15 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
   app.setGlobalPrefix('api');
   app.enableCors();
 
   /* app.use(csurf()); */
   app.use(helmet());
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter as any));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
 }
